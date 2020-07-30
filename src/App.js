5e99b3b290d0fc5f6b3fbadd87d25536a9b06fc2
AngelaT623 <Angela.Taylor87@gmail.com>
2020-07-30 13:20:41 -0400

    add .env file with API key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const key = process.env.REACT_APP_WEATHERAPP_API_KEY;
+  
   return (
     <div className="App">
       <header className="App-header">
